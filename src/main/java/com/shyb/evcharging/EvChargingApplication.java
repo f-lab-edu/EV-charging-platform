f15788827eec8bfecfb625ff1c60e0805d9a2bc9
shinsanghooon <shinsanghooon@gmail.com>
2022-09-08 02:23:53 +0900

    feat: 스프링 프로젝트 세팅
    스프링 프로젝트를 세팅하라
    
    Closes #1, #2

---
+++ src/main/java/com/shyb/evcharging/EvChargingApplication.java
@@ -0,0 +1,13 @@
+package com.shyb.evcharging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvChargingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvChargingApplication.class, args);
+    }
+
+}
