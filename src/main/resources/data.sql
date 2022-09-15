0daf7aff9123bc4d0454840ae145c8a1c722193f
shinsanghooon <shinsanghooon@gmail.com>
2022-09-15 23:22:41 +0900

    [#6] <chore>: data.sql 파일 추가
    1. 기존에 사용하던 도커 초기화 스크립트인 init.sql을 삭제
    2. 데이터 관련 초기화 작업을 더 유연하게 관리하기 위해 data.sql을
       사용하도록 변경
    
    Close [#6]

---
+++ src/main/resources/data.sql
@@ -0,0 +1,38 @@
+CREATE TABLE ev_cars
+(
+    id                    INT PRIMARY KEY AUTO_INCREMENT,
+    ev_car_type              VARCHAR(50),
+    charge_connector_type VARCHAR(50),
+    created_at            DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at            TIMESTAMP,
+    is_deleted            BOOLEAN
+);
+
+CREATE TABLE cards
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    card_number VARCHAR(30),
+    cvc         VARCHAR(10),
+    yy          VARCHAR(2),
+    mm          VARCHAR(2),
+    created_at  DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP,
+    is_deleted  BOOLEAN
+);
+
+CREATE TABLE users
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(20)  NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    phone      VARCHAR(20)  NOT NULL,
+    email      VARCHAR(50)  NOT NULL,
+    ev_car_id     int,
+    foreign key (ev_car_id) references ev_cars (id),
+    card_id    int,
+    foreign key (card_id) references cards (id),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    is_deleted BOOLEAN
+);
+
