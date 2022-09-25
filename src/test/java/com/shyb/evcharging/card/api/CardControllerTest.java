e47104480403b5eafc00fce243c91cce13afa5e4
shinsanghooon <shinsanghooon@gmail.com>
2022-09-25 22:20:16 +0900

    test: 카드 삭제 API 테스트 코드 작성
    1. 카드 삭제 컨트롤러 테스트 코드 작성
    
    Related to #13

--- src/test/java/com/shyb/evcharging/card/api/CardControllerTest.java
+++ src/test/java/com/shyb/evcharging/card/api/CardControllerTest.java
@@ -1,11 +1,15 @@
 package com.shyb.evcharging.card.api;
 
+import static com.shyb.evcharging.utils.CardFixture.CARD_ID;
+import static com.shyb.evcharging.utils.CardFixture.INVALID_CARD_ID;
 import static com.shyb.evcharging.utils.CardFixture.INVALID_MONTH_REQUEST;
 import static com.shyb.evcharging.utils.CardFixture.VALID_CARD_NUMBER;
 import static com.shyb.evcharging.utils.CardFixture.VALID_REQUEST;
 import static com.shyb.evcharging.utils.CardFixture.VALID_RESPONSE;
 import static com.shyb.evcharging.utils.MockMvcUtil.OBJECT_MAPPER;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -14,6 +18,7 @@ import com.shyb.evcharging.card.application.CardService;
 import com.shyb.evcharging.card.dto.CardRequestDto;
 import com.shyb.evcharging.card.dto.CardResponseDto;
 import com.shyb.evcharging.card.exception.CardExceptionHandler;
+import com.shyb.evcharging.user.exception.EmailDuplicateException;
 import com.shyb.evcharging.utils.MockMvcUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -87,4 +92,38 @@ class CardControllerTest {
         }
     }
 
+    @DisplayName("카드 삭제 API는")
+    @Nested
+    class Context_delete_card{
+
+        @DisplayName("카드 삭제가 성공하면 상태코드 200을 리턴한다.")
+        @Test
+        void delete_card() throws Exception {
+
+            ResultActions result = mockMvc.perform(
+                delete("/api/v1/cards/" + CARD_ID)
+                    .contentType(MediaType.APPLICATION_JSON)
+            );
+
+            result
+                .andExpect(status().isOk());
+        }
+
+        @DisplayName("등록되지 않은 카드에 대한 삭제 요청이 오면 상태코드 400과 에러 메시지를  리턴한다.")
+        @Test
+        void delete_card_with_invalid_card() throws Exception {
+
+            doThrow(new IllegalArgumentException("삭제 요청된 카드가 존재하지 않습니다.")).when(cardService).delete(INVALID_CARD_ID);
+
+            ResultActions result = mockMvc.perform(
+                delete("/api/v1/cards/" + INVALID_CARD_ID)
+                    .contentType(MediaType.APPLICATION_JSON)
+            );
+
+            result
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorMessage").exists());
+        }
+
+    }
 }
